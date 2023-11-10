3c1abbf724ead26d0165c22ca358edb8f5fad1b3
Steve <steve.hoareau1@gmail.com>
2023-11-10 16:03:18 +0400

    try to log user to java ee

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\PasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
@@ -35,20 +37,39 @@ class IndexController extends AbstractController
     public function login(Request $request, HttpClientInterface $client): Response
     {
         if($request->request->has('email') && $request->request->has('password')){
-            $client = $client->withOptions([
+            $oneClient = clone $client;
+            $oneClient = $oneClient->withOptions([
                 'auth_basic' => [
                     'username' => $request->request->get('email'),
                     'password' => $request->request->get('password')
                 ],
+                'body'=>'{"email":"'.$request->request->get('email').'"}',
+                "headers"=>array('Content-Type' => 'application/json')
             ]);
-            $response = $client->request(
+            $response = $oneClient->request(
                 'GET',
-                $_ENV['RESTAPI'].'/bank/test',
+                $_ENV['RESTAPI'].'/secret'
             );
             try {
+                $token = $response->getContent();
+                //on récupère le sel du client
+                $oneClient = clone $client;
+                $oneClient = $oneClient->withOptions([
+                    'auth_bearer' => $token,
+                    'body'=>'{"email":"'.$request->request->get('email').'"}',
+                    "headers"=>array('Content-Type' => 'application/json')
+                ]);
+                $response = $oneClient->request(
+                    'GET',
+                    $_ENV['RESTAPI'].'/secret/'.$_ENV['PRIVATE_SALTED_ROOT']
+                );
                 dd($response->getContent());
+                return $this->render('index.html.twig', [
+                    "accountbank"=>["amount"=>750,"iban"=>"FR7165434524251078502727126"],
+                    "user"=>["firstname"=>"Steve","name"=>"HOAREAU"]
+                ]);
             } catch (ClientExceptionInterface|RedirectionExceptionInterface|ServerExceptionInterface|TransportExceptionInterface $e) {
-                dd($e->getMessage());
+                dd($client,$e->getMessage());
             }
         }
         return $this->render("login.html.twig");
